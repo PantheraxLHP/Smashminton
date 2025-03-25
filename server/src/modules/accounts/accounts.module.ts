d4e549f1be7b1826102643e02f3bc37549990e53
Shiphuu <psphu2103@gmail.com>
2025-03-25 19:10:46 +0700

    My first commit

--- server/src/modules/accounts/accounts.module.ts
+++ server/src/modules/accounts/accounts.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
 import { PrismaModule } from '../prisma/prisma.module';
-import { Customer } from '../customer/entities/customer.entity';
+import { CustomerModule } from '../customer/customer.module';
 
 @Module({
     controllers: [AccountsController],
     providers: [AccountsService],
-    imports: [PrismaModule, Customer],
+    imports: [PrismaModule, CustomerModule],
 })
-export class AccountsModule {}
+export class AccountsModule { }
--- server/src/modules/customer/customer.module.ts
+++ server/src/modules/customer/customer.module.ts
@@ -7,5 +7,6 @@ import { PrismaModule } from '../prisma/prisma.module';
     controllers: [CustomerController],
     providers: [CustomerService],
     imports: [PrismaModule],
+    exports: [CustomerService]
 })
 export class CustomerModule {}
