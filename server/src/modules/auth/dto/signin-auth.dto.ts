cc1224299b2c8f9125ddd13e6fa45b50734a017c
duckien2012 <hoanganhtra181@gmail.com>
2025-04-16 14:49:43 +0700

    chore: delete multipart form in signin

--- server/src/modules/auth/dto/signin-auth.dto.ts
+++ server/src/modules/auth/dto/signin-auth.dto.ts
@@ -1,12 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 export class SigninAuthDto {
-    @ApiProperty({ example: 'jkien' })
+    @ApiProperty({ example: 'nguyenvun' })
     @IsString()
     @IsNotEmpty()
     username: string;
 
-    @ApiProperty({ example: 'password123' })
+    @ApiProperty({ example: '123' })
     @IsString()
     @IsNotEmpty()
     password: string;
