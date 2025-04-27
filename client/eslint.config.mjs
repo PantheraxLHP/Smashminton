a3cdb23812527ad0fb8548fbfd95d952b1b2be2a
panthera <leehuynhphuc@gmail.com>
2025-04-27 16:20:46 +0000

    fix: fix prettier error

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -11,10 +11,10 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
     ...compat.config({
-        extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+        extends: ['next/core-web-vitals', 'next/typescript'],
         rules: {
             '@typescript-eslint/no-explicit-any': 'off',
-            'prettier/prettier': ['error', { endOfLine: 'auto' }],
+            '@typescript-eslint/no-unused-vars': 'warn',
         },
     }),
 ];
