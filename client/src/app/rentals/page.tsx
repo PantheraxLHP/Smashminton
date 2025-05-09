c375a44a5c6fe18911942d40d78404729bfab751
zRush1202 <phutrong.ltv@gmail.com>
2025-05-09 23:58:30 +0700

    rental: add bookingbottomsheet

--- client/src/app/rentals/page.tsx
+++ client/src/app/rentals/page.tsx
@@ -3,6 +3,33 @@
 import { useState } from 'react';
 import RentalFilter from './RentalFilter';
 import RentalList from './RentalList';
+import BookingBottomSheet from '../booking/_components/BookingBottomSheet';
+
+type Racket = {
+    productid: number;
+    id: number;
+    name: string;
+    price: number;
+    brand: string;
+    weight: string;
+    stiffness: string;
+    balance: string;
+    quantity: number;
+    productName: string;
+};
+
+type Shoe = {
+    productid: number;
+    id: number;
+    name: string;
+    price: number;
+    brand: string;
+    form: string;
+    size: string;
+    quantity: number;
+    productName: string;
+};
+
 
 export default function RentalsPage() {
     const [selectedCategory, setSelectedCategory] = useState<'Thuê vợt' | 'Thuê giày'>('Thuê vợt');
@@ -15,6 +42,37 @@ export default function RentalsPage() {
         forms: [] as string[],
         sizes: [] as string[],
     });
+
+    const [rentalQuantities, setRentalQuantities] = useState<{ [key: number]: number }>({});
+    const [rackets, setRackets] = useState<Racket[]>([]);
+    const [shoes, setShoes] = useState<Shoe[]>([]);
+
+    const handleIncrement = (id: number) => {
+        setRentalQuantities((prev) => ({
+            ...prev,
+            [id]: (prev[id] || 0) + 1,
+        }));
+    };
+
+    const handleDecrement = (id: number) => {
+        setRentalQuantities((prev) => ({
+            ...prev,
+            [id]: Math.max((prev[id] || 0) - 1, 0),
+        }));
+    };
+
+    const selectedItems = [...rackets, ...shoes]
+        .filter((item) => rentalQuantities[item.productid] > 0)
+        .map((item) => ({
+            ...item,
+            quantity: rentalQuantities[item.productid],
+            productname: item.name,
+        }));
+
+
+    const totalPrice = selectedItems.reduce((sum, item) => {
+        return sum + (item.price || 0) * item.quantity;
+    }, 0);
 
     return (
         <div className="max-w-8xl mx-auto px-4 py-6 md:px-8 lg:px-12">
@@ -34,9 +92,25 @@ export default function RentalsPage() {
                     <RentalList
                         selectedCategory={selectedCategory}
                         filters={filters}
+                        rentalQuantities={rentalQuantities}
+                        onIncrement={handleIncrement}
+                        onDecrement={handleDecrement}
+                        setRackets={setRackets}
+                        setShoes={setShoes}
                     />
                 </main>
             </div>
+
+            {selectedItems.length > 0 && (
+                <BookingBottomSheet
+                    totalPrice={totalPrice}
+                    selectedProducts={selectedItems}
+                    selectedCourts={[]}
+                    TTL={0}
+                    onResetTimer={() => { }}
+                    onConfirm={() => { }}
+                />
+            )}
         </div>
     );
 }
