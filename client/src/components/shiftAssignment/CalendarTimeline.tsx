a557e5e8891c0477714dc865cf5c40adb0d36c01
Tang Duc Phong <ducphongtang1293@gmail.com>
2025-05-12 08:09:32 +0700

    feat(Assignments UI): Add ShiftCard

--- client/src/components/shiftAssignment/CalendarTimeline.tsx
+++ client/src/components/shiftAssignment/CalendarTimeline.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useState, Fragment } from "react";
+import { ShiftDate, ShiftAssignment } from "@/types/types";
+import { getDay, addDays } from "date-fns";
+import ShiftCard from "./ShiftCard";
 
 const CalendarTimeline = () => {
-    const today = new Date().getDay();
+    const today = getDay(new Date());
     const todayIndex = today === 0 ? 6 : today - 1;
     const timesInDay = Array.from({ length: 17 }, (_, i) => `${6 + i}:00`);
     const [topOffset, setTopOffset] = useState(0);
@@ -30,12 +33,49 @@ const CalendarTimeline = () => {
         return () => clearInterval(interval);
     }, []);
 
+    const nextWeekStart = addDays(new Date(), 7 - todayIndex);
+
+    const startTime = ["06:00", "14:00", "06:00", "10:00", "14:00", "18:00"];
+    const endTime = ["14:00", "22:00", "10:00", "14:00", "18:00", "22:00"];
+
+    const tmpShiftDates: ShiftDate[] = [];
+    for (let i = 1; i < 7; i++) {
+        for (let j = 0; j < 7; j++) {
+            tmpShiftDates.push({
+                shiftid: i,
+                shiftdate: addDays(nextWeekStart, j),
+                shift: {
+                    shiftid: i,
+                    shiftstarthour: startTime[i - 1],
+                    shiftendhour: endTime[i - 1],
+                }
+            });
+        }
+    }
+
+    const tmpShiftAssignments: ShiftAssignment[] = [];
+    for (let i = 0; i < 28; i++) {
+        tmpShiftAssignments.push({
+            shiftid: Math.floor(Math.random() * 6 + 1),
+            shiftdate: addDays(nextWeekStart, i % 7),
+            employeeid: i,
+        });
+    }
+
+    const fulltimeShiftDates = tmpShiftDates.filter((shiftDate) => {
+        return shiftDate.shiftid < 3;
+    });
+
+    const parttimeShiftDates = tmpShiftDates.filter((shiftDate) => {
+        return shiftDate.shiftid >= 3;
+    });
+
     return (
         <div className="relative">
             {/*Time indicator line*/}
             {topOffset >= 0 && (
                 <div
-                    className="absolute left-0 right-0 h-[2px] bg-primary z-50"
+                    className="absolute left-0 right-0 h-[2px] bg-primary z-1"
                     style={{ top: `${topOffset}px` }}
                 />
             )}
@@ -55,6 +95,19 @@ const CalendarTimeline = () => {
                     </Fragment>
                 ))}
             </div>
+
+            {/* Shift cards */}
+            <div className="absolute top-[44px] left-0 grid grid-cols-[100px_repeat(7,minmax(0px,_1fr))] w-full">
+                {parttimeShiftDates.map((shiftDate, index) => (
+                    <Fragment key={`shift-${index}`}>
+                        <ShiftCard
+                            shiftDate={shiftDate}
+                            shiftAssignments={[]}
+                        />
+                    </Fragment>
+                ))}
+            </div>
+
         </div>
     );
 };
---
+++ client/src/components/shiftAssignment/ShiftCard.tsx
@@ -0,0 +1,80 @@
+import { ShiftDate, ShiftAssignment } from "@/types/types";
+import { getDay } from "date-fns";
+import { Button } from "@/components/ui/button";
+import { Icon } from "@iconify/react";
+
+const colorIndex = [
+    "bg-yellow-500",
+    "bg-[#008CFF]",
+    "bg-yellow-500",
+    "bg-primary",
+    "bg-[#008CFF]",
+    "bg-[#746C82]"
+];
+const borderColorIndex = [
+    "border-yellow-500 group-hover:border-yellow-500",
+    "border-[#008CFF] group-hover:border-[#008CFF]",
+    "border-yellow-500 group-hover:border-yellow-500",
+    "border-primary group-hover:border-primary",
+    "border-[#008CFF] group-hover:border-[#008CFF]",
+    "border-[#746C82] group-hover:border-[#746C82]"
+];
+const pixelsPerHourH = 44;
+const pixelsPerParttimeShiftH = pixelsPerHourH * 4;
+const pixelsPerFulltimeShiftH = pixelsPerHourH * 8;
+
+const isColStart = (shiftDate: ShiftDate) => {
+    return getDay(shiftDate.shiftdate) === 1 ? true : false;
+}
+
+const getHeight = (shiftDate: ShiftDate) => {
+    if (shiftDate.shiftid < 3) {
+        return pixelsPerFulltimeShiftH;
+    }
+    else {
+        return pixelsPerParttimeShiftH;
+    }
+}
+
+interface ShiftCardProps {
+    shiftDate: ShiftDate;
+    shiftAssignments: ShiftAssignment[];
+}
+
+const ShiftCard: React.FC<ShiftCardProps> = ({
+    shiftDate,
+    shiftAssignments,
+}) => {
+    return (
+        <div
+            className={`group flex flex-col mx-2 cursor-pointer ${isColStart(shiftDate) ? "col-start-2" : ""}`}
+            style={{
+                height: `${getHeight(shiftDate)}px`,
+            }}
+        >
+            <div
+                className={`text-center text-sm font-semibold text-white ${colorIndex[shiftDate.shiftid - 1]} rounded-t-lg p-1 h-fit`}
+            >
+                {`${shiftDate.shift?.shiftstarthour} - ${shiftDate.shift?.shiftendhour}`}
+            </div>
+            <div
+                className={`flex flex-col gap-2 p-2 rounded-b-lg h-full border-l-2 border-b-2 border-r-2 bg-white z-2 hover:${borderColorIndex[shiftDate.shiftid - 1]}`}>
+                {/* <span>WTF1</span>
+                <span>WTF2</span>
+                <span>WTF3</span> */}
+                {shiftAssignments.length > 0 && (
+                    <>
+                    </>
+                )}
+                {shiftAssignments.length === 0 && (
+                    <Button variant="outline_destructive" className="text-sm gap-0.5">
+                        <Icon icon="ic:baseline-plus" className="" />
+                        Cần phân công
+                    </Button>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default ShiftCard;
