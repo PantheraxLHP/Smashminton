5e4ac3927d3bbb71db6d027643545ca699c5e72c
panthera <leehuynhphuc@gmail.com>
2025-05-22 09:57:25 +0000

    update: img in homepage, signin, signup to cloudinary

--- client/src/app/(auth)/signin/page.tsx
+++ client/src/app/(auth)/signin/page.tsx
@@ -1,19 +1,9 @@
-import Image from 'next/image';
 import SignInForm from './SigninForm';
 
 export default function SignInPage() {
     return (
-        <div className="flex min-h-screen flex-col bg-gray-200">
-            <div className="relative flex flex-1 items-center justify-center bg-black">
-                <Image
-                    src="/loginbg.png"
-                    alt="Badminton"
-                    fill
-                    priority
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                    className="absolute inset-0 h-full w-full object-cover opacity-100"
-                />
-
+        <div className="min-h-screen bg-[url('https://res.cloudinary.com/dnagyxwcl/image/upload/v1747907584/loginbg_l2ss1n.jpg')] bg-cover bg-center">
+            <div className="flex min-h-screen items-center justify-center p-8">
                 <SignInForm />
             </div>
         </div>
--- client/src/app/_homepage/hero-section.tsx
+++ client/src/app/_homepage/hero-section.tsx
@@ -1,41 +1,31 @@
 import { Button } from '@/components/ui/button';
-import Image from 'next/image';
-import FastBooking from './fast-booking';
 import Link from 'next/link';
+import FastBooking from './fast-booking';
 
 const HeroSection = () => {
     return (
+        <div className="max-h-screen bg-[url('https://res.cloudinary.com/dnagyxwcl/image/upload/v1746248985/What_is_the_Weight_of_a_Plastic_Shuttlecock__-_BadmintonBites_cwgpxi.jpg')] bg-cover bg-center p-8">
+            <div className="relative max-h-screen w-full">
+                <div className="container mx-auto flex h-full flex-col items-center justify-between px-4 py-16 md:px-6 lg:flex-row lg:px-8">
+                    {/* Left Section: Text */}
+                    <div className="max-w-xl text-center text-white lg:text-left">
+                        <h1 className="text-3xl leading-tight font-bold uppercase drop-shadow-lg md:text-4xl lg:text-5xl">
+                            Thể thao mỗi ngày, sức khỏe mỗi phút
+                        </h1>
+                        <p className="mt-3 text-lg italic drop-shadow-lg md:text-2xl">Đặt sân cầu lông ngay hôm nay!</p>
+                        <Button
+                            variant="default"
+                            className="bg-primary hover:bg-primary/60 mt-6 transform rounded-lg px-6 py-3 text-lg font-bold text-white shadow-lg transition-all hover:scale-105"
+                            asChild
+                        >
+                            <Link href={'booking/courts'}>ĐẶT SÂN NGAY !!!</Link>
+                        </Button>
+                    </div>
 
-        
-        <div className="relative max-h-screen w-full">
-            <Image
-                // src="/homebg.png"
-                src="https://res.cloudinary.com/dnagyxwcl/image/upload/v1746248985/What_is_the_Weight_of_a_Plastic_Shuttlecock__-_BadmintonBites_cwgpxi.jpg"
-                alt="Badminton Court"
-                fill
-                priority
-                quality={100}
-                className="absolute inset-0 -z-10 object-cover"
-            />
-            <div className="container mx-auto flex h-full flex-col items-center justify-between px-4 py-16 md:px-6 lg:flex-row lg:px-8">
-                {/* Left Section: Text */}
-                <div className="max-w-xl text-center text-white lg:text-left">
-                    <h1 className="text-3xl leading-tight font-bold uppercase drop-shadow-lg md:text-4xl lg:text-5xl">
-                        Thể thao mỗi ngày, sức khỏe mỗi phút
-                    </h1>
-                    <p className="mt-3 text-lg italic drop-shadow-lg md:text-2xl">Đặt sân cầu lông ngay hôm nay!</p>
-                    <Button
-                        variant="default"
-                        className="bg-primary hover:bg-primary/60 mt-6 transform rounded-lg px-6 py-3 text-lg font-bold text-white shadow-lg transition-all hover:scale-105"
-                        asChild
-                    >
-                        <Link href={'booking/courts'}>ĐẶT SÂN NGAY !!!</Link>
-                    </Button>
-                </div>
-
-                {/* Right Section: Form */}
-                <div className="max-w-md">
-                    <FastBooking />
+                    {/* Right Section: Form */}
+                    <div className="max-w-md">
+                        <FastBooking />
+                    </div>
                 </div>
             </div>
         </div>
