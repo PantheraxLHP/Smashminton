ff5353279d0c08c6bc87f2a368facc277c20383e
panthera <leehuynhphuc@gmail.com>
2025-06-04 10:23:51 +0000

    feat: update employee

---
+++ client/src/app/api/employees/put-employee/route.ts
@@ -0,0 +1,33 @@
+import { ApiResponse } from '@/lib/apiResponse';
+import { NextRequest } from 'next/server';
+
+export async function PUT(request: NextRequest) {
+    try {
+        const formData = new FormData();
+        const { employeeId, ...body } = await request.json();
+        formData.append('employeeId', employeeId.toString());
+        formData.append('fullname', body.fullname);
+        formData.append('email', body.email);
+        formData.append('phone', body.phone);
+        formData.append('address', body.address);
+        formData.append('position', body.position);
+        formData.append('salary', body.salary.toString());
+        formData.append('avatar', body.avatar);
+        const response = await fetch(`${process.env.SERVER}/api/v1/employees/${employeeId}`, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                credentials: 'include',
+            },
+            method: 'PUT',
+            body: formData,
+        });
+
+        if (!response.ok) {
+            return ApiResponse.error(`HTTP error! Status: ${response.status}`);
+        }
+        const result = await response.json();
+        return ApiResponse.success(result);
+    } catch (error) {
+        return ApiResponse.error(error instanceof Error ? error.message : 'Không thể thực hiện yêu cầu');
+    }
+}
--- client/src/services/employees.service.ts
+++ client/src/services/employees.service.ts
@@ -51,12 +51,51 @@ export const postEmployee = async (employeeData: {
     }
 };
 
+export const putEmployee = async (
+    employeeId: number,
+    employeeData: {
+        fullname?: string;
+        email?: string;
+        phone?: string;
+        address?: string;
+        position?: string;
+        salary?: number;
+        avatar?: File;
+    },
+) => {
+    try {
+        const formData = new FormData();
+        formData.append('employeeId', employeeId.toString());
+        formData.append('fullname', employeeData.fullname || '');
+        formData.append('email', employeeData.email || '');
+        formData.append('phone', employeeData.phone || '');
+        formData.append('address', employeeData.address || '');
+        formData.append('position', employeeData.position || '');
+        formData.append('salary', employeeData.salary?.toString() || '');
+        formData.append('avatar', employeeData.avatar || new Blob());
+        const response = await fetch('/api/employees/put-employee', {
+            method: 'PUT',
+            headers: { 'Content-Type': 'multipart/form-data' },
+            body: formData,
+            credentials: 'include',
+        });
+        const result = await response.json();
+
+        if (!response.ok) {
+            return ServiceResponse.error(result.message || 'Không thể thực hiện yêu cầu');
+        }
+        return ServiceResponse.success(result.data);
+    } catch (error) {
+        return ServiceResponse.error(error instanceof Error ? error.message : 'Không thể thực hiện yêu cầu');
+    }
+};
+
 export const deleteEmployees = async (employeeIds: number[]) => {
     try {
         const response = await fetch('/api/employees/delete-employee', {
             method: 'DELETE',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify( employeeIds ),
+            body: JSON.stringify(employeeIds),
             credentials: 'include',
         });
         const result = await response.json();
