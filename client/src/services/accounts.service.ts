1b667e6e097313a998c41295526b88482631c704
panthera <leehuynhphuc@gmail.com>
2025-06-05 11:33:01 +0000

    feat: Profile Update Password

--- client/src/services/accounts.service.ts
+++ client/src/services/accounts.service.ts
@@ -48,3 +48,35 @@ export const updateProfile = async (accountId: number, formData: FormData) => {
         return ServiceResponse.error(error instanceof Error ? error.message : 'Không thể cập nhật thông tin');
     }
 };
+
+export const updatePassword = async (
+    accountId: number | undefined       ,
+    changedPasswordData: {
+        newPassword?: string;
+        confirmPassword?: string;
+    },
+) => {
+    try {
+        const queryParams = new URLSearchParams();
+        queryParams.set('accountid', accountId?.toString() || '');
+
+        const response = await fetch(`/api/accounts/update-password?${queryParams.toString()}`, {
+            method: 'PUT',
+            body: JSON.stringify(changedPasswordData),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+        });
+
+        const result = await response.json();
+
+        if (!response.ok) {
+            return ServiceResponse.error(result.message || 'Không thể cập nhật thông tin');
+        }
+
+        return ServiceResponse.success(result.data);
+    } catch (error) {
+        return ServiceResponse.error(error instanceof Error ? error.message : 'Không thể cập nhật thông tin');
+    }
+};
