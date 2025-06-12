42ec35bb1ba642bee24f45125008110c92eb38bc
panthera <leehuynhphuc@gmail.com>
2025-06-12 09:15:46 +0000

    feat: load emp not in shift

---
+++ client/src/app/api/shiftdate/search-employees/route.ts
@@ -0,0 +1,23 @@
+import { ApiResponse } from '@/lib/apiResponse';
+import { NextRequest } from 'next/server';
+
+export async function GET(request: NextRequest) {
+    try {
+        const searchParams = request.nextUrl.searchParams;
+        const params = new URLSearchParams(searchParams).toString();
+        const response = await fetch(`${process.env.SERVER}/api/v1/shift-date/employees-not-in-shift?${params}`, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+        });
+
+        if (!response.ok) {
+            return ApiResponse.error(`HTTP error! Status: ${response.status}`);
+        }
+        const result = await response.json();
+        return ApiResponse.success(result);
+    } catch (error) {
+        return ApiResponse.error(error instanceof Error ? error.message : 'Không thể tải danh sách sân');
+    }
+}
