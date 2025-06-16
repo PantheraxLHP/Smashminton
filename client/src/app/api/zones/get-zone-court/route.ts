835033848e63fefef64fc85ccfd1d401378a433e
zRush1202 <phutrong.ltv@gmail.com>
2025-06-16 14:38:44 +0700

    pagination for zone-court

--- client/src/app/api/zones/get-zone-court/route.ts
+++ client/src/app/api/zones/get-zone-court/route.ts
@@ -1,21 +1,23 @@
 import { ApiResponse } from '@/lib/apiResponse';
+import { NextRequest } from 'next/server';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
     try {
-        const response = await fetch(`${process.env.SERVER}/api/v1/zones/all-zones-with-courts`, {
+        const searchParams = request.nextUrl.searchParams;
+        const params = new URLSearchParams(searchParams).toString();
+        const response = await fetch(`${process.env.SERVER}/api/v1/zones/all-zones-with-courts?${params}`, {
             headers: {
                 'Content-Type': 'application/json',
             },
+            credentials: 'include',
         });
 
         if (!response.ok) {
             return ApiResponse.error(`HTTP error! Status: ${response.status}`);
         }
-
-        const data = await response.json();
-        return ApiResponse.success({ zones: data });
+        const result = await response.json();
+        return ApiResponse.success(result);
     } catch (error) {
         return ApiResponse.error(error instanceof Error ? error.message : 'Không thể tải danh sách sân');
     }
 }
-
