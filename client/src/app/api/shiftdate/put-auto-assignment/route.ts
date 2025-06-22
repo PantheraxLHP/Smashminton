7f9a60d5c97ebfc39d33e4ab105d33bb2e2c964a
panthera <leehuynhphuc@gmail.com>
2025-06-22 13:28:37 +0000

    feat: api for ruleList

---
+++ client/src/app/api/shiftdate/put-auto-assignment/route.ts
@@ -0,0 +1,23 @@
+import { ApiResponse } from '@/lib/apiResponse';
+import { NextRequest } from 'next/server';
+
+export async function PUT(request: NextRequest) {
+    try {
+        const body = await request.json();
+
+        const response = await fetch(`${process.env.SERVER}/api/v1/auto-assignment`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body),
+            credentials: 'include',
+        });
+
+        if (!response.ok) {
+            return ApiResponse.error(`HTTP error! Status: ${response.status}`);
+        }
+        const result = await response.json();
+        return ApiResponse.success(result);
+    } catch (error) {
+        return ApiResponse.error(error instanceof Error ? error.message : 'Không thể thực hiện yêu cầu');
+    }
+}
