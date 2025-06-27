45223bef97ece1b50006fb203af486ae7c7c763c
panthera <leehuynhphuc@gmail.com>
2025-06-28 01:19:57 +0700

    feat: load feature zone rating star

--- server/src/modules/zones/zones.service.ts
+++ server/src/modules/zones/zones.service.ts
@@ -9,190 +9,182 @@ import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class ZonesService {
-  constructor(private prisma: PrismaService,
-    private cloudinaryService: CloudinaryService,
-  ) {
+    constructor(
+        private prisma: PrismaService,
+        private cloudinaryService: CloudinaryService,
+    ) {}
 
-  }
+    async getZonesWithCourts(page: number = 1, limit: number = 12) {
+        const now = new Date();
+        const skip = (page - 1) * limit;
 
-  async getZonesWithCourts(page: number = 1, limit: number = 12) {
-    const now = new Date();
-    const skip = (page - 1) * limit;
-
-    const findZones = await this.prisma.zones.findMany({
-      include: {
-        courts: true,
-      },
-      orderBy: {
-        zoneid: 'asc',
-      },
-    });
-
-    // // Tính avgzonerating cho từng zone và update
-    // for (const zone of ) {
-    //   const validRatings = zone.courts
-    //     .map(c => c.avgrating)
-    //     .filter(r => r !== null && r !== undefined) as number[];
-
-    //   const avg = validRatings.length
-    //     ? Number((validRatings.reduce((a, b) => a + +b, 0) / validRatings.length).toFixed(1))
-    //     : 0;
-
-    //   await this.prisma.zones.update({
-    //     where: { zoneid: zone.zoneid },
-    //     data: { avgzonerating: avg },
-    //   });
-
-    //   zones.avgzonerating = avg;
-    // }
-
-    for (const zone of findZones) {
-        const ratings = zone.courts
-            .map((court) => court.avgrating)
-            .filter((r) => r !== null && r !== undefined);
-
-        const avg =
-            ratings.length > 0
-                ? Number(
-                    (
-                        ratings.reduce((sum, r) => sum + Number(r), 0) / ratings.length
-                    ).toFixed(1)
-                )
-                : 0;
-
-        await this.prisma.zones.update({
-            where: { zoneid: zone.zoneid },
-            data: { avgzonerating: avg },
+        const findZones = await this.prisma.zones.findMany({
+            include: {
+                courts: true,
+            },
+            orderBy: {
+                zoneid: 'asc',
+            },
         });
-      zone.avgzonerating = new Prisma.Decimal(avg);;
+
+        // // Tính avgzonerating cho từng zone và update
+        // for (const zone of ) {
+        //   const validRatings = zone.courts
+        //     .map(c => c.avgrating)
+        //     .filter(r => r !== null && r !== undefined) as number[];
+
+        //   const avg = validRatings.length
+        //     ? Number((validRatings.reduce((a, b) => a + +b, 0) / validRatings.length).toFixed(1))
+        //     : 0;
+
+        //   await this.prisma.zones.update({
+        //     where: { zoneid: zone.zoneid },
+        //     data: { avgzonerating: avg },
+        //   });
+
+        //   zones.avgzonerating = avg;
+        // }
+
+        for (const zone of findZones) {
+            const ratings = zone.courts.map((court) => court.avgrating).filter((r) => r !== null && r !== undefined);
+
+            const avg =
+                ratings.length > 0
+                    ? Number((ratings.reduce((sum, r) => sum + Number(r), 0) / ratings.length).toFixed(1))
+                    : 0;
+
+            await this.prisma.zones.update({
+                where: { zoneid: zone.zoneid },
+                data: { avgzonerating: avg },
+            });
+        }
+
+        const zones = await findZones.map((zone) => ({
+            zoneid: zone.zoneid,
+            zonename: zone.zonename,
+            zonetype: zone.zonetype,
+            zoneimgurl: zone.zoneimgurl,
+            zonedescription: zone.zonedescription,
+            avgzonerating: zone.avgzonerating,
+            courts: zone.courts.map((court) => ({
+                courtid: court.courtid,
+                courtname: court.courtname,
+                courtstatus: court.statuscourt,
+                courtimgurl: court.courtimgurl,
+                courtavgrating: court.avgrating,
+                courttimecalculateavg: court.timecalculateavg,
+            })),
+        }));
+
+        const total = zones.length;
+        const totalPages = Math.ceil(total / limit);
+
+        const paginatedZones = zones.slice(skip, skip + limit);
+
+        return {
+            data: paginatedZones,
+            pagination: {
+                page: page,
+                totalPages: totalPages,
+            },
+        };
     }
 
-    const zones = await findZones.map(zone => ({
-      zoneid: zone.zoneid,
-      zonename: zone.zonename,
-      zonetype: zone.zonetype,
-      zoneimgurl: zone.zoneimgurl,
-      zonedescription: zone.zonedescription,
-      avgzonerating: zone.avgzonerating,
-      courts: zone.courts.map(court => ({
-        courtid: court.courtid,
-        courtname: court.courtname,
-        courtstatus: court.statuscourt,
-        courtimgurl: court.courtimgurl,
-        courtavgrating: court.avgrating,
-        courttimecalculateavg: court.timecalculateavg,
-      })),
-    }));
+    async create(createZoneDto: CreateZoneDto, file: Express.Multer.File) {
+        let url_zone: string = '';
+        if (file) {
+            // If files are provided, upload them to Cloudinary
+            const uploadResults = await this.cloudinaryService.uploadZoneImg(file); // Changed to handle multiple files
+            url_zone = uploadResults.secure_url || '';
+            if (!url_zone) {
+                throw new BadRequestException('Failed to upload files');
+            }
+        }
 
-    const total = zones.length;
-    const totalPages = Math.ceil(total / limit);
+        createZoneDto.zoneimgurl = url_zone;
 
-    const paginatedZones = zones.slice(skip, skip + limit);
+        // Update account details in the database
+        const createZone = await this.prisma.zones.create({
+            data: createZoneDto,
+        });
 
-    return {
-      data: paginatedZones,
-      pagination: {
-        page: page,
-        totalPages: totalPages
-      },
+        if (!createZone) {
+            throw new BadRequestException('Failed to update account');
+        }
+
+        // Danh sách 4 block zone_price cứng như hình mày gửi
+        const zonePriceData = [
+            {
+                dayfrom: 'Monday',
+                dayto: 'Friday',
+                starttime: '06:00',
+                endtime: '08:00',
+                price: 200000,
+            },
+            {
+                dayfrom: 'Monday',
+                dayto: 'Friday',
+                starttime: '08:00',
+                endtime: '16:00',
+                price: 200000,
+            },
+            {
+                dayfrom: 'Monday',
+                dayto: 'Friday',
+                starttime: '16:00',
+                endtime: '22:00',
+                price: 200000,
+            },
+            {
+                dayfrom: 'Saturday',
+                dayto: 'Sunday',
+                starttime: '06:00',
+                endtime: '22:00',
+                price: 200000,
+            },
+        ];
+
+        // Tạo 4 zone_prices kèm theo zone mới
+        await Promise.all(
+            zonePriceData.map((zonePrice) =>
+                this.prisma.zone_prices.create({
+                    data: {
+                        ...zonePrice,
+                        zoneid: createZone.zoneid,
+                    },
+                }),
+            ),
+        );
+
+        return {
+            message: 'Zone and default prices created',
+            zone: createZone,
+        };
     }
-  }
 
-
-  async create(createZoneDto: CreateZoneDto, file: Express.Multer.File) {
-    let url_zone: string = '';
-    if (file) {
-      // If files are provided, upload them to Cloudinary
-      const uploadResults = await this.cloudinaryService.uploadZoneImg(file); // Changed to handle multiple files
-      url_zone = uploadResults.secure_url || '';
-      if (!url_zone) {
-        throw new BadRequestException('Failed to upload files');
-      }
+    findAll(): Promise<Zones[]> {
+        const getAllZones = this.prisma.zones.findMany({
+            select: {
+                zoneid: true,
+                zonename: true,
+                zonetype: true,
+                zoneimgurl: true,
+                zonedescription: true,
+                avgzonerating: true,
+            },
+        });
+        return getAllZones;
     }
 
-    createZoneDto.zoneimgurl = url_zone;
-
-    // Update account details in the database
-    const createZone = await this.prisma.zones.create({
-      data: createZoneDto,
-    });
-
-    if (!createZone) {
-      throw new BadRequestException('Failed to update account');
+    findOne(id: number) {
+        return `This action returns a #${id} zone`;
     }
 
-    // Danh sách 4 block zone_price cứng như hình mày gửi
-    const zonePriceData = [
-      {
-        dayfrom: 'Monday',
-        dayto: 'Friday',
-        starttime: '06:00',
-        endtime: '08:00',
-        price: 200000,
-      },
-      {
-        dayfrom: 'Monday',
-        dayto: 'Friday',
-        starttime: '08:00',
-        endtime: '16:00',
-        price: 200000,
-      },
-      {
-        dayfrom: 'Monday',
-        dayto: 'Friday',
-        starttime: '16:00',
-        endtime: '22:00',
-        price: 200000,
-      },
-      {
-        dayfrom: 'Saturday',
-        dayto: 'Sunday',
-        starttime: '06:00',
-        endtime: '22:00',
-        price: 200000,
-      },
-    ];
+    update(id: number, updateZoneDto: UpdateZoneDto) {
+        return `This action updates a #${id} zone`;
+    }
 
-    // Tạo 4 zone_prices kèm theo zone mới
-    await Promise.all(
-      zonePriceData.map((zonePrice) =>
-        this.prisma.zone_prices.create({
-          data: {
-            ...zonePrice,
-            zoneid: createZone.zoneid,
-          },
-        }),
-      ),
-    );
-
-    return {
-      message: 'Zone and default prices created',
-      zone: createZone,
-    };
-  }
-
-  findAll(): Promise<Zones[]> {
-    const getAllZones = this.prisma.zones.findMany({
-      select: {
-        zoneid: true,
-        zonename: true,
-        zonetype: true,
-        zoneimgurl: true,
-        zonedescription: true,
-      }
-    });
-    return getAllZones;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} zone`;
-  }
-
-  update(id: number, updateZoneDto: UpdateZoneDto) {
-    return `This action updates a #${id} zone`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} zone`;
-  }
+    remove(id: number) {
+        return `This action removes a #${id} zone`;
+    }
 }
